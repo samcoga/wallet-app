e41226b9a441ddbe64467f3ba4d349e57d473525
samcoga <samcoga@gmail.com>
2016-04-24 20:24:08 -0500

    models and basic files

---
+++ routes/routes.js
@@ -0,0 +1,33 @@
+var appRouter = function(app) {
+    //home test
+    app.get("/", function(req, res) {
+        res.send("Hello World");
+    });
+ 
+    //getting account info
+    app.get("/account", function(req, res) {
+        var accountMock = {
+            "username": "nraboy",
+            "password": "1234",
+            "twitter": "@nraboy"
+        }
+        if(!req.query.username) {
+            return res.send({"status": "error", "message": "missing username"});
+        } else if(req.query.username != accountMock.username) {
+            return res.send({"status": "error", "message": "wrong username"});
+        } else {
+            return res.send(accountMock);
+        }
+    });
+    
+    //creating accounts
+    app.post("/account", function(req, res) {
+        if(!req.body.username || !req.body.password || !req.body.twitter) {
+            return res.send({"status": "error", "message": "missing a parameter"});
+        } else {
+            return res.send(req.body);
+        }
+    });
+}
+
+module.exports = appRouter;
